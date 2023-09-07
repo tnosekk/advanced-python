99b7cdce2465b6b308f6c15be2b93052000437a8
tnosek <tnosek@student.wszib.edu.pl>
2023-09-07 14:12:19 +0200

    learning python

--- Start/Ch_5/capture_start.py
+++ Start/Ch_5/capture_start.py
@@ -6,3 +6,7 @@ name = input("What is your name? ")
 match name:
     case "":
         print("Hello, anonymous!")
+    case "Joe" | "Joseph" as s:
+        print(f"Hi there {s}")
+    case name:
+        print(f"Hello, {name}")
--- Start/Ch_5/simple_start.py
+++ Start/Ch_5/simple_start.py
@@ -1,6 +1,18 @@
 # Example file for Advanced Python: Language Features by Joe Marini
 # Simple pattern matching using literal values
 
-x = 0
+x = 1
 
 # TODO: Literal patterns are explicit values: integers, strings, Booleans, etc
+
+match x:
+    case 0:
+        print("Zero")
+    case 1:
+        print("One")
+    case "Zero":
+        print(0)
+    case None:
+        print("Nothing")
+    case _:
+        print("no match")
